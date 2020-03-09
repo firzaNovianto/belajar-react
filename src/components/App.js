018fc37a6a4154e0e58b81a03344d7a879345013
FirzaNovianto <virza56@gmail.com>
2020-03-09 10:10:48 +0700

    belajar react

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class App extends React.Component {
+
+    render () {
+        return (
+            <div>
+                <h1>REACT 101</h1>
+            </div>
+        )
+    }
+}
+
+export default App
